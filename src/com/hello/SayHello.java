0f75d49936e8e13f12d10cdcba8a366f17a1b674
LVHAO <levuhao2495@gmail.com>
2017-05-25 15:04:11 +0700

    add comment
    Change-Id: I1fa285011a82ebd29b0fd812b864b31f9570de5f
    Signed-off-by: LVHAO <levuhao2495@gmail.com>

--- src/com/hello/SayHello.java
+++ src/com/hello/SayHello.java
@@ -5,6 +5,7 @@ public class SayHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello .........");
+		System.out.println("Add comment continuous");
 	}
 
 }
